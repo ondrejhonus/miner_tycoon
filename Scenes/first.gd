d8032c1d5727f285317adda279ba46d343a5b15f
ondrejhonus <honusondrej@gmail.com>
2024-06-17 11:22:21 +0200

    not funtioning save logic, but its not far i hope

--- Scenes/first.gd
+++ Scenes/first.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+const SAVE_DIR := "user://save.dat"
+
 var coins = Big.new(0.001)
 var cps = Big.new(0)
 var click_power = Big.new(1,0)
@@ -67,14 +69,91 @@ var terminal_price = Big.new(31,16)
 var terminal_power = Big.new(15,10)
 var terminal_count = 0
 
+ 
+func save():
+	var save_dict = {
+	"filename" : get_scene_file_path(),
+	"parent" : get_parent().get_path(),
+	"playtime": playtime_seconds
+	}
+	return save_dict
+	
+func save_game():
+	var save_game = FileAccess.open("user://savegame.save", FileAccess.WRITE)
+	var save_nodes = get_tree().get_nodes_in_group("Persist")
+	for node in save_nodes:
+		# Check the node is an instanced scene so it can be instanced again during load.
+		if node.scene_file_path.is_empty():
+			print("persistent node '%s' is not an instanced scene, skipped" % node.name)
+			continue
+
+		# Check the node has a save function.
+		if !node.has_method("save"):
+			print("persistent node '%s' is missing a save() function, skipped" % node.name)
+			continue
+
+		# Call the node's save function.
+		var node_data = node.call("save")
+
+		# JSON provides a static method to serialized JSON string.
+		var json_string = JSON.stringify(node_data)
+
+		# Store the save dictionary as a new line in the save file.
+		save_game.store_line(json_string)
+
+# Note: This can be called from anywhere inside the tree. This function
+# is path independent.
+func load_game():
+	if not FileAccess.file_exists("user://savegame.save"):
+		print("There is no save to load")
+		return # Error! We don't have a save to load.		
+
+	# We need to revert the game state so we're not cloning objects
+	# during loading. This will vary wildly depending on the needs of a
+	# project, so take care with this step.
+	# For our example, we will accomplish this by deleting saveable objects.
+	var save_nodes = get_tree().get_nodes_in_group("Persist")
+	for i in save_nodes:
+		i.queue_free()
+
+	# Load the file line by line and process that dictionary to restore
+	# the object it represents.
+	var save_game = FileAccess.open("user://savegame.save", FileAccess.READ)
+	while save_game.get_position() < save_game.get_length():
+		var json_string = save_game.get_line()
+
+		# Creates the helper class to interact with JSON
+		var json = JSON.new()
+
+		# Check if there is any error while parsing the JSON string, skip in case of failure
+		var parse_result = json.parse(json_string)
+		if not parse_result == OK:
+			print("JSON Parse Error: ", json.get_error_message(), " in ", json_string, " at line ", json.get_error_line())
+			continue
+
+		# Get the data from the JSON object
+		var node_data = json.get_data()
+
+		# Firstly, we need to create the object and add it to the tree and set its position.
+		var new_object = load(node_data["filename"]).instantiate()
+		get_node(node_data["parent"]).add_child(new_object)
+
+		# Now we set the remaining variables.
+		for i in node_data.keys():
+			if i == "filename" or i == "parent":
+				continue
+			new_object.set(i, node_data[i])
+
 func _ready():
+	load_game()
 	Big.setSmallDecimals(1)
 	Big.setThousandDecimals(2)
 	Big.setBigDecimals(3)
 	$Canvas/ClickingRect/RichTextLabel.text =  "[center]%s coins
 %s cps[/center]" % [coins.toMetricSymbol(), cps.toMetricSymbol()]
 	pass
- 
+
+
 func playtime(total_seconds: float) -> String:
 	var seconds:float = fmod(total_seconds , 60.0)
 	var minutes:int   =  int(total_seconds / 60.0) % 60
@@ -87,6 +166,11 @@ func playtime(total_seconds: float) -> String:
 	return timer
 
 func _process(delta):
+	# Save game every minute
+	if (playtime_seconds % 60.0 == 0):
+		$Canvas/Notification.text = "[center]Game saved automatically[/center]"
+		save_game()
+		
 # Coin system
 	coins.plusEquals(cps.times(delta))
 	$Canvas/ClickingRect/RichTextLabel.text = "[center]%s coins
